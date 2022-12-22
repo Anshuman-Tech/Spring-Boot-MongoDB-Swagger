15943d44c656a84fd97d536e8d52ddc75448800d
Anshuman Singh <anshuman20november@gmail.com>
2022-12-23 02:48:57 +0530

    Added model,controller,service,and repository layers. Created getALLJobs API

--- src/main/java/com/anshuman/springbootmongo/controller/JobController.java
+++ src/main/java/com/anshuman/springbootmongo/controller/JobController.java
@@ -1,16 +1,31 @@
 package com.anshuman.springbootmongo.controller;
 
+import com.anshuman.springbootmongo.model.Job;
+import com.anshuman.springbootmongo.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 public class JobController {
 
+    @Autowired
+    JobService jobService;
+
+    @ApiIgnore
     @RequestMapping(value = "/")
     public void redirect(HttpServletResponse response) throws IOException {
         response.sendRedirect("/swagger-ui.html");
+    }
+
+    @GetMapping("/jobs")
+    public List<Job> getJobs(){
+        return jobService.getAllJobs();
     }
 }
